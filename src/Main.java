bc4439d741a3262373da526e452c77a0a9e97902
Student <cwalker1332@gmail.com>
2020-10-30 12:36:42 -0500

    create game class & update Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // start rock, paper, scissors
+        System.out.println("Let's play rock, paper, scissors! First one to win three rounds is the winner!");
+        Game newGame = new Game();
+	    newGame.startGame();
     }
 }
